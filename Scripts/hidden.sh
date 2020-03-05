b9ea7684a9614a52d42867488f75133ac2a20a3d
Miles Young <mwyoung2@gmail.com>
2020-03-04 21:17:33 -0800

    Update deskew and hidden with optargs

--- Scripts/hidden.sh
+++ Scripts/hidden.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
-if [[ "$1" != d* ]]; then
-	find ../ -type f -name "._*" 
-else
-	echo "Deleting"
-	find ../ -type f -name "._*" -delete
-	#find ../ -type f -name "._*" | xargs -0 rm -r 
-	#find ../ -type f -name "._*" | sed 1d | xargs rm -r 
+usage() { echo "Usage: $0 -f (compile/hidden files) -d (show empty directories) -r (remove)
+    example: $0 -f -r       removes compiled file names" 1>&2; exit 1;}
+while getopts "hdfr" arg; do
+    case "${arg}" in
+        d) d=1 ;;
+        f) f=1 ;;
+        r) r=1 ;;
+        h) usage; exit 0 ;;
+    esac
+done
+shift $(($OPTIND-1)) #remove optargs
+
+function delete_files {
+    find . \( -name '._*' -or -name '*.sw[lmnop]' -or -name '*.bin' -or -name '*.hex' \
+    -or -name '*.lst' -or -name '*.map' -or -name '*.srec' -or -name '*.raw' \) \
+    -type f $1
+}
+
+if [[ -n "${f}" ]]; then
+    delete_files "-print"
+    if [[ -n "${r}" ]]; then
+        echo "\tDeleting files"
+        delete_files "-delete"
+    fi
+fi
+
+function delete_empty_dir {
+    find . -type d -not -path "*/.git/*" -empty "$1"
+}
+
+if [[ -n "${d}" ]]; then
+    delete_empty_dir "-print"
+    if [[ -n "${r}" ]]; then
+        echo -e "\tDeleting empty directories"
+        delete_empty_dir "-delete"
+    fi
 fi
