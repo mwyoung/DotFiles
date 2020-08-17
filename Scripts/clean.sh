f3a2ffcad3695d6872acab01d7707d5ee8f2db4b
Miles Young <mwyoung2@gmail.com>
2020-08-16 17:17:51 -0700

    Added function to rename all files to text, fixed help menu

--- Scripts/clean.sh
+++ Scripts/clean.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-usage() { echo "Usage: $0 -f (compile/hidden files) -d (show empty directories) -r (remove)
-    example: $0 -f -r       removes compiled file names" 1>&2; exit 1;}
-while getopts "hdfr" arg; do
+usage() { echo "Usage: $0 -f (compile/hidden files) -d (show empty directories) -r (remove) -t (to text file)
+    example: $0 -f -r       removes compiled file names" 1>&2;}
+while getopts "hdfrt" arg; do
     case "${arg}" in
         d) d=1 ;;
         f) f=1 ;;
         r) r=1 ;;
+        t) t=1 ;;
         h) usage; exit 0 ;;
     esac
 done
+if [ $OPTIND -eq 1 ]; then
+    usage; exit 0;
+fi
 shift $(($OPTIND-1)) #remove optargs
 
 function delete_files {
     find . \( -name '._*' -or -name '*.sw[lmnop]' -or -name '*.bin' -or -name '*.hex' \
-    -or -name '*.lst' -or -name '*.map' -or -name '*.srec' -or -name '*.raw' \) \
+    -or -name '*.lst' -or -name '*.map' -or -name '*.srec' -or -name '*.raw' \
+    -or -name "*.elf" -or -name "*.o" \) \
     -type f $1
 }
 
@@ -35,4 +40,8 @@ if [[ -n "${d}" ]]; then
         echo -e "\tDeleting empty directories"
         delete_empty_dir "-delete"
     fi
+fi
+
+if [[ -n "${t}" ]]; then
+    find . -type f -name '*' -execdir mv {} {}.txt ';'
 fi
